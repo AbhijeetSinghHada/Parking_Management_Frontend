ed64da22f3e0558bff20e5f0d49ee07cd0359b92
Abhijeet Singh Hada <abhi22hada@gmail.com>
2023-12-11 16:30:03 +0530

    removed Wait in auth

--- src/app/Interceptors/auth-interceptor.service.ts
+++ src/app/Interceptors/auth-interceptor.service.ts
@@ -42,10 +42,3 @@ export class AuthInterceptorService implements HttpInterceptor {
     );
   }
 }
-function wait(ms) {
-  var start = Date.now(),
-    now = start;
-  while (now - start < ms) {
-    now = Date.now();
-  }
-}
